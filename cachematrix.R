0db0ad450a89d4d20343b97b28ed1e56ea058eb8
tsecc <freddytcchen@gmail.com>
2018-07-01 18:22:40 +0800

    add code for inversing matrix and comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,28 @@
-## Test
+## The R script has 2 functions that can cache Matrix and Inversed Matrix
 
-## Write a short comment describing this function
-
+## The function defined 4 methods: set/get value, set/get inversed matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(solve) m <<- solve
+  getInverse <- function() m
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## Check if an inversed matrix already exists, if not, set it and cache it
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  if(!is.null(m)){
+    message("inverse matrix exists!")
+    return(m)
+  }
+  data<-x$get()
+  m<-solve(data)
+  x$setInverse(m) #this line caches the result in x, avoid repeat computation
+  m
 }
